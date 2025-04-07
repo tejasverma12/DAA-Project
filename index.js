e7ebd80048495cffb2cb1bde8fd8ba000f57e7f5
tejasverma12 <tejasverma2000@gmail.com>
2025-04-07 15:49:07 +0530

    Add files via upload
    This project is a simple web-based implementation of the 0/1 Knapsack Problem and  Fibonacci series using HTML, CSS and JavaScript.

---
+++ index.js
@@ -0,0 +1,18 @@
+function solveKnapsack() {
+    window.location.href = 'Knapsack.html';
+}
+
+function solveCoinChange() {
+    window.location.href = 'coinchange.html';
+}
+
+function generateFibonacci() {
+    window.location.href = 'fibonnaci.html'
+}
+
+function multiplyMatrices() {
+    window.location.href = 'Matrix.html'
+}
+document.getElementById("knapsackBtn").addEventListener("click", solveKnapsack);
+document.getElementById("fibonacciBtn").addEventListener("click", generateFibonacci);
+
